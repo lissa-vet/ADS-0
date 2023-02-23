ed80c72e55260f4fe2ef7ec9822f30482273038c
lissa-vet <125082940+lissa-vet@users.noreply.github.com>
2023-02-23 20:21:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,8 @@
 
 int gcd(int a, int b) {
     int nod = 0;
-
-    for (int i = 1; a > i || b > i; i++)
-    {
+    
+    for (int i = 1; a > i || b > i; i++) {
         if ((a % i == 0) && (b % i == 0)) 
             nod = i;
         i++;
